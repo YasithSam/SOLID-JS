c1ee0b7f739c26ddb96b58c7ae5ceb5e3b4d1086
YasithSam <yasith.sam7@gmail.com>
2021-12-10 10:47:19 +0530

    add

---
+++ open-closed.js
@@ -0,0 +1,29 @@
+// class RangeQuestion{
+//     constructor(){
+
+//     }
+// }
+// class BooleanQuestion{
+//     constructor(){
+        
+//     }
+// }
+// class MCQQuestion{
+//     constructor(){
+        
+//     }
+// }
+
+// function printQuiz(questions){
+//     questions.forEach(question => {
+//         console.log(question.description)
+//         question.printQuestionChoice()
+//     })
+// }
+
+
+// const questions=[
+//  new BooleanQuestion(''),
+//  new RangeQuestion(''),
+
+// ]
