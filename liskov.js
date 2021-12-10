a06a2db2fe0e0d54b4152356389bb33ce1112bf9
YasithSam <yasith.sam7@gmail.com>
2021-12-10 10:47:02 +0530

    add

---
+++ liskov.js
@@ -0,0 +1,34 @@
+// class FlyingBird{
+//     fly(){
+
+//     }
+// }
+// class SwimmingBird{
+//     swim(){
+
+//     }
+// }
+// class Penguin extends SwimmingBird{
+//     fly(){
+
+//     }
+// }
+// class Duck extends FlyingBird{
+//     fly(){
+        
+//     }
+// }
+
+// function makeFlyingBirdFly(bird){
+
+// }
+
+// function makeSwimmingBirdFly(bird){
+    
+// }
+
+// const duck=new Duck()
+// const penguin=new Penguin()
+
+// makeFlyingBirdFly(duck)
+// makeSwimmingBirdFly(penguin)
